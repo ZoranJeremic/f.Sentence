1c5b4993d8ab5154894a3b6280086fe935b0200b
Zoran Jeremić <batajeremiccc@gmail.com>
2025-05-30 18:52:04 +0200

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 22
         targetSdk = 35
-        versionCode = 0.0.2
-        versionName = 2
+        versionCode = 2
+        versionName = "0.0.2"
     }
 
     buildTypes {
