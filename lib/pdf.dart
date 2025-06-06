664d03d050df545c74ba33d88106179fa2d564a8
Zoran Jeremić <batajeremiccc@gmail.com>
2025-06-06 16:55:44 +0200

    Update pdf.dart

--- lib/pdf.dart
+++ lib/pdf.dart
@@ -1,1 +1,11 @@
-// blank
+import 'package:flutter/material.dart';
+
+class PDFScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('PDF')),
+      body: Center(child: Text('PDF editing is coming soon!')),
+    );
+  }
+}
