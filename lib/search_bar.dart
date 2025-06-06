1dd33e835f9450d7f1a43c119055313c3a4ff234
Zoran Jeremić <batajeremiccc@gmail.com>
2025-06-06 16:53:07 +0200

    Update search_bar.dart

--- lib/search_bar.dart
+++ lib/search_bar.dart
@@ -1,1 +1,11 @@
-// blank
+import 'package:flutter/material.dart';
+
+class SearchBarWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Search')),
+      body: Center(child: Text('Search screen coming soon!')),
+    );
+  }
+}
