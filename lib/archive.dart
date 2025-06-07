0bb9445d9ec175566b28224bfb796dfeb5fefd34
Zoran Jeremić <batajeremiccc@gmail.com>
2025-06-07 20:20:03 +0200

    Update archive.dart

--- lib/archive.dart
+++ lib/archive.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
-class ArchiveScreen extends StatelessWidget { 
-  const FavoritesScreen({super.key}); 
+class ArchiveScreen extends StatelessWidget {
+  const ArchiveScreen({super.key}); // Konstruktor se zove kao klasa!
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Archive'),
+        title: const Text('Archive'), // Obavezno const
       ),
-      body: const Center(child: Text('Archive is coming soon!')),
+      body: const Center(child: Text('Archive Screen Content')), // Obavezno const
     );
   }
 }
