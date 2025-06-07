b96e978bda8d0a82d94b0e20a99f70dfe5f53f0a
Zoran Jeremić <batajeremiccc@gmail.com>
2025-06-07 18:23:44 +0200

    Update favorites.dart

--- lib/favorites.dart
+++ lib/favorites.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 
-class FavoritesScreen extends StatelessWidget {
+class FavoritesScreen extends StatelessWidget { 
+  const FavoritesScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Favorites)),
-      body: Center(child: Text('Favorites are coming soon!')),
+      appBar: AppBar(
+        title: const Text('Favorites'), 
+      ),
+      body: const Center(child: Text('Favorites are coming soon!')), 
     );
   }
 }
