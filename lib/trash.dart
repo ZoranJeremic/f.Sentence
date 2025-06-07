7e3a72c64128c12c344fe252102c873506900ccd
Zoran Jeremić <batajeremiccc@gmail.com>
2025-06-07 18:26:50 +0200

    Update trash.dart

--- lib/trash.dart
+++ lib/trash.dart
@@ -1,11 +1,16 @@
+// Primer za 'favorites.dart'
 import 'package:flutter/material.dart';
 
-class TrashScreen extends StatelessWidget {
+class TrashScreen extends StatelessWidget { // Pazi da je ime klase ispravno (npr. FavoritesScreen)
+  const FavoritesScreen({super.key}); // OVDE DODAJ const
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Trash')),
-      body: Center(child: Text('Trash is coming soon!')),
+      appBar: AppBar(
+        title: const Text('Favorites'), // OVDE DODAJ const
+      ),
+      body: const Center(child: Text('Favorites are coming soon!')), // OVDE DODAJ const
     );
   }
 }
