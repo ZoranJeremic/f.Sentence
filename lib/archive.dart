a56a60680c9c17211e891029cb87a67e62cfdcdf
Zoran Jeremić <batajeremiccc@gmail.com>
2025-06-07 15:37:09 +0200

    Create archive.dart

---
+++ lib/archive.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FoldersScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Archive')),
+      body: Center(child: Text('Archive is coming soon!')),
+    );
+  }
+}
