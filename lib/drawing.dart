c82b7ca499074f41001a37e6942535828b7d37dd
Zoran Jeremić <batajeremiccc@gmail.com>
2025-06-07 18:21:51 +0200

    Update drawing.dart

--- lib/drawing.dart
+++ lib/drawing.dart
@@ -1,11 +1,16 @@
+// Primer za 'favorites.dart'
 import 'package:flutter/material.dart';
 
 class DrawingScreen extends StatelessWidget {
+  const FavoritesScreen({super.key}); 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Drawing')),
-      body: Center(child: Text('Drawings are coming soon!')),
+      appBar: AppBar(
+        title: const Text('Drawings'), 
+      ),
+      body: const Center(child: Text('Drawings are coming soon!')),
     );
   }
 }
