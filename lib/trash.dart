96e1c5ff397aaeaef54f4a62055e952020c5dc11
Zoran Jeremić <batajeremiccc@gmail.com>
2025-06-07 15:34:21 +0200

    Create trash.dart

---
+++ lib/trash.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TrashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Notes')),
+      body: Center(child: Text('Trash is coming soon!')),
+    );
+  }
+}
