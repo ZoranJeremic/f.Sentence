1411e9d0b50b515e9b561c69991bbfb96175dcff
Zoran Jeremić <batajeremiccc@gmail.com>
2025-06-07 18:26:26 +0200

    Update settings.dart

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,11 +1,16 @@
+// Primer za 'favorites.dart'
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatelessWidget { // Pazi da je ime klase ispravno (npr. FavoritesScreen)
+  const FavoritesScreen({super.key}); // OVDE DODAJ const
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Settings')),
-      body: Center(child: Text('Settings are coming soon!')),
+      appBar: AppBar(
+        title: const Text('Favorites'), // OVDE DODAJ const
+      ),
+      body: const Center(child: Text('Favorites are coming soon!')), // OVDE DODAJ const
     );
   }
 }
