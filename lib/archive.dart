a08419b621084e0b2c839e85a7b4af0efe781786
Zoran Jeremić <batajeremiccc@gmail.com>
2025-06-07 15:38:09 +0200

    Update archive.dart

--- lib/archive.dart
+++ lib/archive.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class FoldersScreen extends StatelessWidget {
+class ArchiveScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
