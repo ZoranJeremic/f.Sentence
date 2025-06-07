e2c80e343d0fb63d73b20155a5f1b93862648294
Zoran Jeremić <batajeremiccc@gmail.com>
2025-06-07 15:29:23 +0200

    Create favorites.dart

---
+++ lib/favorites.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FavoritesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Notes')),
+      body: Center(child: Text('Favorites are coming soon!')),
+    );
+  }
+}
