40c52d068c57f50a32079d1aeb1a0d3b123b29f7
Zoran Jeremić <batajeremiccc@gmail.com>
2025-06-07 18:24:43 +0200

    Update pdf.dart

--- lib/pdf.dart
+++ lib/pdf.dart
@@ -1,11 +1,16 @@
+// Primer za 'favorites.dart'
 import 'package:flutter/material.dart';
 
-class PDFScreen extends StatelessWidget {
+class FavoritesScreen extends StatelessWidget { // Pazi da je ime klase ispravno (npr. FavoritesScreen)
+  const FavoritesScreen({super.key}); // OVDE DODAJ const
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('PDF')),
-      body: Center(child: Text('PDF editing is coming soon!')),
+      appBar: AppBar(
+        title: const Text('Favorites'), // OVDE DODAJ const
+      ),
+      body: const Center(child: Text('Favorites are coming soon!')), // OVDE DODAJ const
     );
   }
 }
