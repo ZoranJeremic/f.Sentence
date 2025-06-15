f1658768eb4c0046a2e639e2e590c6a6f92d7d7a
Zoran Jeremić <batajeremiccc@gmail.com>
2025-06-15 20:56:39 +0200

    Update notes_screen.dart

--- lib/notes_screen.dart
+++ lib/notes_screen.dart
@@ -59,7 +59,7 @@ class _NotesScreenState extends State<NotesScreen> {
   }
 
   void _unsetHeader() {
-    _controller.formatSelection(const quill.Attribute<String?>('header', 'block', null));
+    _controller.formatSelection(const quill.Attribute<String?>('header', quill.AttributeScope.BLOCKS, null));
   }
 
   @override
